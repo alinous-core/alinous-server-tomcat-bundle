ae02879fdb28b0261b15f4a1766d8356eb716921
iizuka <iizuka@opem-ec.jp>
2014-08-29 11:38:20 +0900

    add ALINOUS_HOME

---
+++ apache-tomcat-7.0.55/ALINOUS_HOME/api/addcookie.js
@@ -0,0 +1,11 @@
+
+function alinousAddCookie(key, value, expireDays)
+{
+	var extime = new Date().getTime();
+	var expire = new Date(extime + (60*60*24*1000*expireDays));
+	
+	document.cookie = key + '=' + encodeURIComponent(value) + '; expires=' + expire.toUTCString()
+		+ "; path=/";
+}
+
+alinousAddCookie('alinous_access_log', '##__VALUE__##', 365);
